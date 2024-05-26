7fe9a7fc6db00c769e05b1db1efb95282ad4136a
NIKITA <rent-n@bk.ru>
2024-05-26 23:56:02 +0300

    the process of creating a user

--- website/website/urls.py
+++ website/website/urls.py
@@ -25,3 +25,4 @@ urlpatterns = [
     path('news/', include('news.urls')),
     path('user/', include('users.urls', namespace='user')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
