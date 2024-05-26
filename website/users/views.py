8f32f8efcfa0e98766058e098885cc0fc62a7960
NIKITA <rent-n@bk.ru>
2024-05-27 01:45:09 +0300

    ready login

--- website/users/views.py
+++ website/users/views.py
@@ -1,12 +1,31 @@
+from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .forms import UserLoginForm
+
 
 # Create your views here.
 
 
 def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Home - Авторизация'
+        'title': 'Home - Авторизация',
+        'form': form,
     }
     return render(request, 'users/login.html', context)
 
