02eae3050c712a2eae89fc7b18c43e2b12b22ee9
NIKITA <rent-n@bk.ru>
2024-05-27 00:45:19 +0300

    before deleting the database

--- website/website/settings.py
+++ website/website/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
