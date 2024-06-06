3a7754e5dd17ceae61024dd735e9edafc086b881
NIKITA <rent-n@bk.ru>
2024-06-06 19:39:06 +0300

    rename task v1

--- website/main/static/main/js/task.js
+++ website/main/static/main/js/task.js
@@ -14,6 +14,31 @@ function addTask(button) {
     const tasksContainer = button.nextElementSibling;
     tasksContainer.appendChild(taskContainer);
 }
+
+function toggleMenu(element) {
+    const menu = element.nextElementSibling;
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
+function renameColumn(element) {
+    const column = element.closest('.column');
+    const header = column.querySelector('.task-header h2');
+    const newName = prompt("Введите новое название колонки:", header.textContent);
+    if (newName) {
+        header.textContent = newName;
+    }
+    element.parentElement.style.display = 'none';
+}
+
+function createNewColumn() {
+    const template = document.getElementById('column-template').content.cloneNode(true);
+    document.querySelector('.columns-container').appendChild(template);
+}
+
 // function addNewTask(button) {
 //     const tasksContainer = button.nextElementSibling;
 //     const newTask = document.createElement('div');
